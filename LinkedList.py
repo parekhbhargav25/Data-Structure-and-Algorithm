4a2a1b35d974da89fc6fc101765fef2f15c45895
parekhbhargav25 <parekhbhargav25@gmail.com>
2023-11-26 00:19:46 -0500

    linked list

---
+++ LinkedList.py
@@ -0,0 +1,230 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__ (self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def printList (self):
+        temp = self.head
+        while(temp is not None ):
+            print(temp.value)
+            temp = temp.next
+    
+
+    def append (self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail.next= new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    
+    def pop(self):
+        pre = self.head
+        temp = self.head
+
+        if self.length == 0: 
+            return None
+        while(temp.next is not None):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+
+        self.length -= 1
+        if (self.length  == 0):
+            self.head = None
+            self.tail =None
+        return temp
+    def prepend(self,value):
+        new_node = Node (value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else: 
+             new_node.next = self.head
+             self.head = new_node
+        self.length += 1
+        return True
+    
+    def PopFirst(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head =  self.head.next
+        temp.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.tail = None
+        return temp
+    def get (self, index):
+        if index < 0 or index == self.length:
+            return None
+        temp = self.head
+        for _ in range (index):
+            temp = temp.next
+        return temp
+        
+        
+
+
+
+
+        
+
+myLinked_list = LinkedList(1)
+myLinked_list.append(2)
+
+
+myLinked_list.prepend(5)
+
+
+
+        
+
+
+
+
+
+
+# class Node:
+#     def __init__(self,value):
+#         self.value=value
+#         self.next = None
+
+
+
+
+# class LinkedList:
+#     def __init__(self,value):
+#         new_node = Node(value)
+#         self.head = new_node
+#         self.tail = new_node
+#         self.length = 1
+    
+#     def print_list (self):
+#         temp = self.head
+#         while temp is not None:
+#             print(temp.value)
+#             temp = temp.next
+
+    # def append (self, value):
+    #     new_node = Node(value)
+    #     if self.head is None:
+    #         self.head = new_node
+    #         self.tail = new_node
+    #     else:
+    #         self.tail.next= new_node
+    #         self.tail = new_node
+    #     self.length += 1
+    #     return True
+#     def pop(self):
+#         pre = self.head
+#         temp = self.head
+#         if self.length ==0:
+#             return None
+        
+#         # For two or more nodes
+#         while(temp.next is not None):
+#             pre = temp
+#             temp = temp.next
+#         self.tail = pre
+#         self.tail.next = None
+#         self.length -= 1
+
+#         if self.length == 0:
+#             self.head = None
+#             self.tail = None
+#         return temp
+
+
+# myLinked_list = LinkedList(1)
+# myLinked_list.append(2)
+
+# myLinked_list.print_list()
+
+# print(myLinked_list.pop())
+# print(myLinked_list.pop())
+# print(myLinked_list.pop())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # class Node:
+# #     def __init__ (self,value):
+# #         self.value = value
+# #         self.next = None
+
+# # class LinkedList:
+# #     def __init__(self,value):
+# #         new_node = Node(value)
+# #         self.head = new_node
+# #         self.tail = new_node
+# #         self.length = 1
+
+
+# # my_linked_lst = LinkedList(4)
+
+# # print(my_linked_lst.head.value)
